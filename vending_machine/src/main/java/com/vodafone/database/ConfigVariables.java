5147ddd09d1bde236072d56ed6708d4442422c00
shehab kotb <shehabktob@gmail.com>
2023-06-13 17:45:52 +0300

    add docker compose file for service

--- vending_machine/src/main/java/com/vodafone/database/ConfigVariables.java
+++ vending_machine/src/main/java/com/vodafone/database/ConfigVariables.java
@@ -2,6 +2,8 @@ package com.vodafone.database;
 
 public enum ConfigVariables {
     // TODO
+
+    // DB_URL("jdbc:mysql://db:3306/payment"), docker version
     DB_URL("jdbc:mysql://127.0.0.1:3306/payment"),
     DB_USER("root"),
     DB_PASSWORD("P@ssw0rd");
